c26d0da54168b33504b7999e8a0de8e566c66b7d
Victor Carnavali <victorcarnavali.dev@gmail.com>
2025-01-08 12:18:59 -0300

    if, else if, using in js

---
+++ study/JavaScript/ex009.js
@@ -0,0 +1,8 @@
+var age = 22;
+if (age < 16) {
+    console.log(`You cant vote`);
+} else if (age < 18 || age > 65){
+        console.log(`Opcional vote`);
+  } else {
+    console.log(`Vote`);
+  }
